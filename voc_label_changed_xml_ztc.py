d1888e7284b2a6bfd2b2b633bc753a1218f7acc4
yuanyupeng0000 <yuanyupeng-1989@163.com>
2018-12-15 21:13:22 +0800

    haha

--- voc_label_changed_xml_ztc.py
+++ voc_label_changed_xml_ztc.py
@@ -23,7 +23,8 @@ def convert(size, box):
     return (x,y,w,h)
 
 def convert_annotation(year, image_id):
-    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
+    #in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
+    in_file = open('Changed_Annotations/%s.xml'%(image_id))
     out_file = open('VOCdevkit/VOC%s/labels/%s.txt'%(year, image_id), 'w')
     tree=ET.parse(in_file)
     root = tree.getroot()
